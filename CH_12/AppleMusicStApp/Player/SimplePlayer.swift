2fe62c19cb990ae5528f04fb54f24d376b8a0743
나성희 <sungh2na@gmail.com>
2020-09-03 05:46:54 +0900

    SimplePlayer 구현
    플레이어뷰 SimplePlayer Class 구현

--- CH_12/AppleMusicStApp/Player/SimplePlayer.swift
+++ CH_12/AppleMusicStApp/Player/SimplePlayer.swift
@@ -9,51 +9,51 @@
 import AVFoundation
 
 class SimplePlayer {
-    // TODO: 싱글톤 만들기, 왜 만드는가?
-    
+    // TODO: 싱글톤 만들기, 왜 만드는가?  -> 여기저기서 쓸 수 있게 함
+    static let shared = SimplePlayer()
     
     private let player = AVPlayer()
 
     var currentTime: Double {
         // TODO: currentTime 구하기
-        return 0.0
+        return player.currentItem?.currentTime().seconds ?? 0
     }
     
     var totalDurationTime: Double {
         // TODO: totalDurationTime 구하기
-        return 0.0
+        return player.currentItem?.duration.seconds ?? 0
     }
     
     var isPlaying: Bool {
         // TODO: isPlaying 구하기
-        return false
+        return player.isPlaying         // extension에 구현
     }
     
     var currentItem: AVPlayerItem? {
         // TODO: currentItem 구하기
-        return nil
+        return player.currentItem
     }
     
     init() { }
     
     func pause() {
         // TODO: pause구현
-        
+        player.pause()
     }
     
     func play() {
         // TODO: play구현
-        
+        player.play()
     }
     
     func seek(to time:CMTime) {
         // TODO: seek구현
-        
+        player.seek(to: time)
     }
     
     func replaceCurrentItem(with item: AVPlayerItem?) {
         // TODO: replace current item 구현
-        
+        player.replaceCurrentItem(with: item)
     }
     
     func addPeriodicTimeObserver(forInterval: CMTime, queue: DispatchQueue?, using: @escaping (CMTime) -> Void) {
