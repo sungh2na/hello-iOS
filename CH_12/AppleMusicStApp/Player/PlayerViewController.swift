b40e63550a2e9aff32d195534c0194a36d4897e0
나성희 <sungh2na@gmail.com>
2020-09-05 23:48:58 +0900

    시간 레이블 업데이트
    재생시 시간 레이블 업데이트

--- CH_12/AppleMusicStApp/Player/PlayerViewController.swift
+++ CH_12/AppleMusicStApp/Player/PlayerViewController.swift
@@ -24,7 +24,7 @@ class PlayerViewController: UIViewController {
     //TODO: SimplePlayer 만들고 프로퍼티 추가
     let simplePlayer = SimplePlayer.shared
     
-    var timeObserver: Any?
+    var timeObserver: Any?      // 시간을 관찰함. 플레이어에서 곡 재생할때 재생되는 것의 시간을 관찰
     var isSeeking: Bool = false
     
     override func viewDidLoad() {       // 메모리에 로드 됐을 때 호출
@@ -33,6 +33,10 @@ class PlayerViewController: UIViewController {
         updatePlayButton()
         updateTime(time: CMTime.zero)
         // TODO: TimeObserver 구현
+        // 일정 간격으로 시간 관찰하는 것 더함
+//        CMTime(seconds: 1, preferredTimescale: 10) // AVFoundation 밑에 있음, 단위 초를 쪼개고 싶은 양으로 표현, 1초를 10개로 나눔 -> 0.1초
+        timeObserver = simplePlayer.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 10), queue: DispatchQueue.main, using: { time
+            in self.updateTime(time: time) })    // 해당하는 곡의 현재시간을 받아서 업데이트
     }
     
     override func viewWillAppear(_ animated: Bool) {    // 보이기 직전에 호출
@@ -90,8 +94,8 @@ extension PlayerViewController {
         // currentTime label, totalduration label, slider
         
         // TODO: 시간정보 업데이트, 심플플레이어 이용해서 수정
-        currentTimeLabel.text = secondsToString(sec: 0.0)   // 3.1234 >> 00:03
-        totalDurationLabel.text = secondsToString(sec: 0.0)  // 39.2045  >> 00:39
+        currentTimeLabel.text = secondsToString(sec: simplePlayer.currentTime)   // 3.1234 >> 00:03
+        totalDurationLabel.text = secondsToString(sec: simplePlayer.totalDurationTime)  // 39.2045  >> 00:39
         
         if isSeeking == false {
             // 노래 들으면서 시킹하면, 자꾸 슬라이더가 업데이트 됨, 따라서 시킹아닐때마 슬라이더 업데이트하자
