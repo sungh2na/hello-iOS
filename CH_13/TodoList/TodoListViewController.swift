b11670cb51cb4a3267db95ce0913cd12458b760d
나성희 <sungh2na@gmail.com>
2020-09-16 01:23:38 +0900

    todo 추가하기, 체크버튼, 삭제버튼 구현
    마무리!

--- CH_13/TodoList/TodoListViewController.swift
+++ CH_13/TodoList/TodoListViewController.swift
@@ -53,6 +53,15 @@ class TodoListViewController: UIViewController {
         // TODO: Todo 태스크 추가
         // add task to view model
         // and tableview reload or update
+        
+        guard let detail = inputTextField.text, detail.isEmpty == false else { return }
+        let todo = TodoManager.shared.createTodo(detail: detail, isToday: isTodayButton.isSelected)
+        
+        todoListViewModel.addTodo(todo)
+        collectionView.reloadData()
+        inputTextField.text = ""
+        isTodayButton.isSelected = false
+        
     }
     
     // TODO: BG 탭했을때, 키보드 내려오게 하기
@@ -111,6 +120,18 @@ extension TodoListViewController: UICollectionViewDataSource {
         // TODO: todo 를 이용해서 updateUI
         // TODO: doneButtonHandler 작성
         // TODO: deleteButtonHandler 작성
+        
+        cell.doneButtonTapHandler = { isDone in     // let isDone = checkButton.isSelected
+            todo.isDone = isDone
+            self.todoListViewModel.updateTodo(todo)
+            self.collectionView.reloadData()
+        }
+        
+        cell.deleteButtonTapHandler = {
+            self.todoListViewModel.deleteTodo(todo)
+            self.collectionView.reloadData()
+        }
+        
         return cell
     }
     
