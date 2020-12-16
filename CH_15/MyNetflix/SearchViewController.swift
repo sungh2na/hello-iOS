2c5265982b196485cf16b01ff2527e1bdcd860a1
나성희 <sungh2na@gmail.com>
2020-12-16 21:19:15 +0900

    SearchAPI 구현

---
+++ CH_15/MyNetflix/SearchViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SearchViewController.swift
+//  MyNetflix
+//
+//  Created by joonwon lee on 2020/04/02.
+//  Copyright © 2020 com.joonwon. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var resultCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    private func dismissKeyboard() {
+        searchBar.resignFirstResponder()    // 첫번째 응답을 그만두게 함.  
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // 검색 시작
+        
+        // 키보드가 올라와 있을 때, 내려가게 처리
+        dismissKeyboard()
+        
+        // 검색어가 있는지
+        guard let searchTerm = searchBar.text, searchTerm.isEmpty == false else { return }
+        // 네트워킹을 통한 검색
+        
+        print("---> 검색어: \(searchBar.text)")
+    }
+}
