37f9412eee34fe543433f26321cf39686421e7c7
나성희 <sungh2na@gmail.com>
2020-12-21 03:31:02 +0900

    플레이어뷰 UI구성

--- CH_15/MyNetflix/PlayerViewController.swift
+++ CH_15/MyNetflix/PlayerViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlayerViewController: UIViewController {
-
+    
+    @IBOutlet weak var playerView: PlayerView!
+    @IBOutlet weak var controlView: UIView!
+    @IBOutlet weak var playButton: UIButton!
+    let player = AVPlayer()
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return .landscapeRight
     }
@@ -19,14 +25,19 @@ class PlayerViewController: UIViewController {
     }
     
 
+    @IBAction func togglePlayButton(_ sender: Any) {
+        playButton.isSelected = !playButton.isSelected
+    }
+    
     @IBAction func closeButtonTapped(_ sender: Any) {
         dismiss(animated: false, completion: nil)
     }
 }
 
-//extension AVPlayer {
-//    var isPlaying: Bool {
-//        guard self.currentItem != nil else { return false }
-//        return self.rate != 0
-//    }
-//}
+extension AVPlayer {
+    var isPlaying: Bool {
+        guard self.currentItem != nil else { return false }
+        return self.rate != 0
+    }
+}
+
