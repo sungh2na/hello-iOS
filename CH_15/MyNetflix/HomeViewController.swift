232a407d1339136986503ff744589b79bb102bb0
나성희 <sungh2na@gmail.com>
2020-12-23 23:08:55 +0900

    오늘의 영화 구현

--- CH_15/MyNetflix/HomeViewController.swift
+++ CH_15/MyNetflix/HomeViewController.swift
@@ -41,18 +41,24 @@ class HomeViewController: UIViewController {
     
 
     @IBAction func playButtonTapped(_ sender: Any) {
-//        SearchAPI.search("interstella") { movies in
-//            guard let interstella = movies.first else { return }
-//            DispatchQueue.main.async {
-//                let url = URL(string: interstella.previewURL)!
-//                let item = AVPlayerItem(url: url)
-//                
-//                let sb = UIStoryboard(name: "Player", bundle: nil)
-//                let vc = sb.instantiateViewController(identifier: "PlayerViewController") as! PlayerViewController
-//                vc.modalPresentationStyle = .fullScreen
-//                vc.player.replaceCurrentItem(with: item)
-//                self.present(vc, animated: false, completion: nil)
-//            }
-//        }
+        // totoro 에 대한 정보를 검색API로 가져온다
+        // 거기서 totoro 객체 하나를 가져온다
+        // 그 객체를 이용해서 PlayerViewController를 띄운다
+        
+        SearchAPI.search("totoro") { movies in
+            guard let totoro = movies.first else { return }
+            
+            DispatchQueue.main.async {
+                let url = URL(string: totoro.previewURL)!
+                let item = AVPlayerItem(url: url)
+                let sb = UIStoryboard(name: "Player", bundle: nil)
+                let vc = sb.instantiateViewController(identifier: "PlayerViewController") as! PlayerViewController
+                vc.player.replaceCurrentItem(with: item)
+                
+                vc.modalPresentationStyle = .fullScreen
+                self.present(vc, animated: false, completion: nil)
+            }
+        }
+        
     }
 }
